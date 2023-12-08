c4009f6ef5e903711a5ebf67e2dd6658060b27c4
Radiktolk <151833196+Radiktolk@users.noreply.github.com>
2023-12-08 14:59:21 +0300

    Update MainWindow.xaml.cs

--- Radik-227-5/MainWindow.xaml.cs
+++ Radik-227-5/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace kochergin_225_5
+namespace Radik_227_5
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
